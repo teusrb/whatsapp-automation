e708e738bbd882e4a776fd500d80388284a4693f
Mateus Rodrigues Baraçal <53863282+teusrb@users.noreply.github.com>
2021-04-05 20:40:41 -0300

    Update whatsapp.py
    Colocando euros na requisição da api de cotação e utilizando o módulo time para termos a data e horário das consultas

--- whatsapp automation/whatsapp.py
+++ whatsapp automation/whatsapp.py
@@ -1,20 +1,26 @@
 from twilio.rest import Client 
 import requests
 import json
+import time
+
 
  
 account_sid = '[Account_Sid]' 
 auth_token = '[Auth_Token]' 
 client = Client(account_sid, auth_token) 
 
-requisicao = requests.get('https://economia.awesomeapi.com.br/all/USD-BRL')
+requisicao = requests.get('https://economia.awesomeapi.com.br/all/USD-BRL,EUR-BRL')
 cotacao = json.loads(requisicao.text)
-valor = float((cotacao['USD'] ['high']))
+valor_dolar = float((cotacao['USD'] ['high']))
+valor_euro = float((cotacao['EUR'] ['high']))
+
+hora = time.strftime('%d/%m/%y %H:%M', time.localtime())
+
 
 def send_message(): 
     message = client.messages.create( 
                               from_='whatsapp:+14155238886',  
-                              body='A cotação atual do dólar é de R$ {:.2f}'.format(valor),      
+                              body='A cotação atual do dólar é de R$ {:.2f} \nA cotação atual do Euro é de R$ {:.2f} \nA data e horário dessa consulta foram: {}'.format(valor_dolar, valor_euro, hora),      
                               to='whatsapp:+5511980283113' 
                           ) 
  
